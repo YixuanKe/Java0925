1d1f39f014940faf81cec42cb88352532450d6b5
puppy19948383@gmail.com <puppy19990223>
2017-10-02 16:05:39 +0800

    fourth

---
+++ src/mile.java
@@ -0,0 +1,17 @@
+public class mile {
+    public static void (String[]args){
+        java.util.Scanner sc1 =new java.util.Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        int Sum = A+B;
+        int sub = A-B;
+        int Mul = A*B;
+
+
+        System.out.println(Sum + " " + Sub + "  " + Mul);
+
+
+    }
+}
